f5adc4f7827aae71df841ff761cd42432e699731
MaPePeR <mapeper@example.com>
2014-02-16 12:30:44 +0100

    BaseMod2

---
+++ ScrollsModLoader.Interfaces/BaseMod2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Mono.Cecil;
+namespace ScrollsModLoader.Interfaces
+{
+	public delegate void BeforeInvokeDelegate(InvocationInfo info);
+	public delegate void AfterInvokeDelegate(InvocationInfo info, ref object returnValue);
+	public delegate bool WantsToReplaceDelegate(InvocationInfo roinfo);
+	public delegate void ReplaceDelegate(InvocationInfo info, out object returnValue);
+	public interface IHookInformation {
+		void addBeforeInvokeHook(MethodDefinition m, BeforeInvokeDelegate hook);
+		void addAfterInvokeHook(MethodDefinition m, BeforeInvokeDelegate hook);
+		void addReplaceHook(MethodDefinition m, WantsToReplaceDelegate conditionDelegate, ReplaceDelegate hook);
+		TypeDefinitionCollection getTypes();
+		int getExeVersion ();
+	}
+	public class BaseMod2
+	{
+		public BaseMod2 (IHookInformation hookInformation)
+		{
+			//So extending Classes also have this Constructor
+		}
+
+		public abstract void Init ();
+	}
+}
+
