f5ab55345bb16ce09c3550072a0ba30ad4140af6
MaPePeR <mapeper@example.com>
2014-02-16 12:13:21 +0100

    ModAssemblyAttributes

---
+++ ScrollsModLoader.Interfaces/ModAssemblyAttributes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ScrollsModLoader.Interfaces
+{
+
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class ModName : Attribute
+	{
+		string name;
+		public ModName () : this ("UnnamedMod") {
+		}
+		public ModName(string s) {
+			name = s;
+		}
+	}
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class ModAuthor : Attribute
+	{
+		string author;
+		public ModAuthor () : this ("Unnamed Author") {
+		}
+		public ModAuthor(string s) {
+			author = s;
+		}
+	}
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class ModVersion : Attribute
+	{
+		int version;
+		public ModVersion () : this (0) {
+		}
+		public ModVersion(int version) {
+			this.version = version;
+		}
+	}
+}
