a3bb3adc49c60352ad8894cdafe22dc56b49642d
MaPePeR <mapeper@example.com>
2014-02-16 12:23:19 +0100

    ModApiVersion-Attribute and readonly-variables

--- ScrollsModLoader.Interfaces/ModAssemblyAttributes.cs
+++ ScrollsModLoader.Interfaces/ModAssemblyAttributes.cs
@@ -2,34 +2,46 @@
 
 namespace ScrollsModLoader.Interfaces
 {
-
 	[AttributeUsage(AttributeTargets.Assembly)]
 	public class ModName : Attribute
 	{
-		string name;
+		public readonly string name;
 		public ModName () : this ("UnnamedMod") {
 		}
 		public ModName(string s) {
 			name = s;
 		}
 	}
+
 	[AttributeUsage(AttributeTargets.Assembly)]
 	public class ModAuthor : Attribute
 	{
-		string author;
+		public readonly string author;
 		public ModAuthor () : this ("Unnamed Author") {
 		}
 		public ModAuthor(string s) {
 			author = s;
 		}
 	}
+
 	[AttributeUsage(AttributeTargets.Assembly)]
 	public class ModVersion : Attribute
 	{
-		int version;
+		public readonly int version;
 		public ModVersion () : this (0) {
 		}
 		public ModVersion(int version) {
+			this.version = version;
+		}
+	}
+
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class ModApiVersion : Attribute
+	{
+		public readonly int version;
+		public ModApiVersion () : this (0) {
+		}
+		public ModApiVersion(int version) {
 			this.version = version;
 		}
 	}
